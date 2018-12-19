30805c2a22ab771c96d0a33bda39195d6581f4fd
Steve Roberts <mbudm.com@gmail.com>
2018-12-19 19:19:36 +1100

    more sketching out

--- src/buyingGroup.ts
+++ src/buyingGroup.ts
@@ -17,6 +17,7 @@ const buyersColumnMap = {
 const ORDER_FORM_SHEET_NAME = 'Order Form';
 const ITEMS_SHEET_NAME = 'Items';
 const BUYERS_SHEET_NAME = 'Buyers';
+const ADMINS_SHEET_NAME = 'Admin users';
 
 function onOpen() {
     var spreadsheet = SpreadsheetApp.getActive();
@@ -29,12 +30,16 @@ function onOpen() {
 
 function createInvoices_(){
     const orderFormData = getSheetData(ORDER_FORM_SHEET_NAME);
-    const invoices = createInvoiceData(orderFormData);
+    const invoicesData = createInvoiceData(orderFormData);
+    const admins = getSheetData(ADMINS_SHEET_NAME);
+    // write a new sheet for each invoice
+    invoicesData.forEach((invoice) => createInvoiceSheet(invoice, admins));
 }
 
 function createOrderSheet_(){
-    var sheet = SpreadsheetApp.getActiveSheet();
-    sheet.getRange('A1').setValues([['createInvoices_ called']]);
+    const itemData = getSheetData(ITEMS_SHEET_NAME);
+    const buyerData = getSheetData(BUYERS_SHEET_NAME);
+    const orderFormData = createOrderFormData(itemData, buyerData);
 }
 
 function getSheetData(sheetName){
@@ -44,6 +49,31 @@ function getSheetData(sheetName){
     return namedSheet.getDataRange()
 }
 
+function createNewSheet(name, data, protections){
+    /*
+    prompt to overwrite if sheet exists?
+    */
+}
+
+function createInvoiceSheet(invoice, admins){
+    const users = admins.concat(invoice[0][1]); 
+    const name = invoice[0][0];
+    createNewSheet(name, invoice, users);
+}
+
 export function createInvoiceData(orderFormData){
+    /*
+    Add friendly name and email as header
+    get the column for this buyer
+    for each row, 
+        filter if no items
+        map to Item, Share size, Share cost, Purchased, Totals
+        Calc total due (validate against otal row?)
+    Add footer info
+    */
     return orderFormData;
+}
+
+export function createOrderFormData(itemData, buyerData){
+    return { itemData, buyerData }
 }
